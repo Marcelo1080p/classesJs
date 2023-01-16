016d0268b52e91fc83b6290721085a58abc83225
Marcelo1080p <juniooor.2015@gmail.com>
2023-01-16 13:18:04 -0300

    herdando os dados de Property em Apartment e House

--- Herança/index.js
+++ Herança/index.js
@@ -8,3 +8,32 @@ class Property {
         return this.price / this.area;
     }
 }
+
+
+class House extends Property {
+    
+}
+
+const land = new Property(200, 50000);
+const someHouse = new House(120, 200000);
+
+console.log(land);
+console.log(someHouse.getPriceSquareMeter());
+console.log( someHouse instanceof Property )
+
+
+class Apartment extends Property{
+    constructor( number, area, price ) {
+        super( area, price );
+        this.number = number;
+    }
+
+    getFloor() {
+        return this.number.slice( 0, -2 );
+    }
+}
+
+
+const apt = new Apartment( "291", "120", "50000" )
+console.log( apt )
+console.log( apt.getFloor() )
